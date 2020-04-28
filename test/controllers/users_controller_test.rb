848958339a64e2e708309ee8d86cb44fb182d647
Timur Emirov <timur.emirow@yandex.ru>
2020-04-28 21:07:12 +0300

    Some updates

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
+
+  # setup do
+  #   get home_path
+  #   sign_in users(:user_001)
+  #   post user_session_url
+  # end
+  #
+  # test "should get show" do
+  #   get users_show_url
+  #   assert_response :success
+  # end
 
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,18 +3,18 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   setup do
+    @user = users(:user_001)
     get home_path
     sign_in users(:user_001)
     post user_session_url
-
-    follow_redirect!
-    assert_response :success
   end
 
   test "layout links" do
     get root_path
     assert_template 'home/index'
+    assert_select "a[href=?]", new_user_session_path, count: 0
     assert_select "a[href=?]", root_path
-    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", destroy_user_session_path
   end
 end
