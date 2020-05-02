4cc4c638a6528aa259318f9d7279c177e02d7269
Timur Emirov <timur.emirow@yandex.ru>
2020-05-02 19:02:18 +0300

    show all users

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+}
+
   #get 'users/index'
   root to: 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get '/home',      to: 'home#index'
   get '/help',      to: 'home#help'
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations'
 
-}
-  resources :users, :only => [:show]
+  resources :users, :only => [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,24 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  include Devise::Test::IntegrationHelpers
-
-  setup do
-    get home_path
-    sign_in users(:user_001)
-    post user_session_url
-
-    follow_redirect!
-    assert_response :success
-  end
-
   test "should get index" do
-    get home_path
-    assert_response :success
-  end
-
-  test "should get help" do
-    get help_path
+    get home_index_url
     assert_response :success
   end
 
